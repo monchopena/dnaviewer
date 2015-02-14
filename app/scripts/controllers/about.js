988358e3da32c9c1cdda172aaed34aa566f1a5a0
Moncho Pena <moncho@bdunk.com>
2015-02-14 09:11:33 +0100

    Yo Angular Base

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dnaviewerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dnaviewerApp
+ */
+angular.module('dnaviewerApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dnaviewerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dnaviewerApp
+ */
+angular.module('dnaviewerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
